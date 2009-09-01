faa65b2217fae928b987180af90102f1eb35f809
pybt.se <pybt.se@32e7d3a0-871e-11de-b29f-cbf71568b5fc>
2009-09-01 15:29:19 +0000

    D    branches/1.0.0b

--- branches/1.0.0b/irg/doc/api_doc.php
+++
@@ -1,9 +0,0 @@
-<?php
-chdir("../../../");
-require("./include/auth.php");
-include("./include/top_graph_header.php");
-
-chdir("plugins/irg");
-include("menu.php");
-?>
-<h1>IRG API Documentation</h1>
--- branches/1.0.0b/irg/irg.php
+++
@@ -1,9 +0,0 @@
-<?php
-chdir('../../');
-
-require("include/auth.php");
-include("./include/top_graph_header.php");
-require("IRG.php");
-
-include("menu.php");
-?>
